15476aa74c7d5443a4839e9635356ca6c4027e42
tony85212 <66199423+tony85212@users.noreply.github.com>
2020-06-11 19:01:50 +0800

    Add files via upload

--- dfa.py
+++ dfa.py
@@ -1,60 +1,81 @@
-from pysmt.shortcuts import Symbol, Or, And, Not, Equals, Int, get_model, is_sat, Solver
+from pysmt.shortcuts import Symbol, Or, And, Not, Equals, get_model, is_sat, Solver
 from pysmt.typing import INT
 import string
 import random
 import sys
+import time
+
 
 class DFA:
 
-    current_state = None;
+    current_state = None
 
     def __init__(self, states, alphabet, transition_function, start_state, accept_states):
 
-        self.states = states;
-        self.alphabet = alphabet;
-        self.transition_function = transition_function;
-        self.start_state = start_state;
-        self.accept_states = accept_states;
-        self.current_state = start_state;
-
-        return;
+        self.states = states
+        self.alphabet = alphabet
+        self.transition_function = transition_function
+        self.start_state = start_state
+        self.accept_states = accept_states
+        self.current_state = start_state
+        return
 
     def transition_to_state_with_input(self, input_value):
 
         if ((self.current_state, input_value) not in self.transition_function.keys()):
+            self.current_state = None
+            return
 
-            self.current_state = None;
-
-            return;
-
-        self.current_state = self.transition_function[(self.current_state, input_value)];
-
-        return;
+        self.current_state = self.transition_function[(self.current_state, input_value)]
+        return
 
 
     def in_accept_state(self):
 
-        return self.current_state;# in accept_states;
+        return self.current_state# in accept_states;
 
     def go_to_initial_state(self):
 
-        self.current_state = self.start_state;
-
-        return;
+        self.current_state = self.start_state
+        return
 
     def run_with_input_list(self, input_list):
 
-        self.go_to_initial_state();
-
+        self.go_to_initial_state()
         for inp in input_list:
+            self.transition_to_state_with_input(inp)
+            continue
+        return self.in_accept_state()
 
-            self.transition_to_state_with_input(inp);
+def DFA_generator(num_states, num_alphabet):
+    states = [i for i in range(num_states)]
+    alphabet = list(string.ascii_lowercase[0:num_alphabet])
 
-            continue;
+    tf = dict()
+    for i in states:
+        for j in alphabet:
+            tf[(i, j)] = random.choice(states)
 
-        return self.in_accept_state();
+    return DFA(states, alphabet, tf, 0, states)
 
-    pass;
+def output_dfa(d):
+    print( len(d.states), len(d.alphabet) )
+    for key, value in d.transition_function.items():
+        print(key[0], key[1], value)
+
+def enumarate_input(alphabet, c):
+    l = []
+    def enumarate_recursive(alphabet, str, c):
+        if(c == 1):
+            for i in alphabet:
+                tmp = str + i
+                l.append(tmp)
+        else:
+            for i in alphabet:
+                tmp = str + i
+                enumarate_recursive(alphabet, tmp, c-1)
+    enumarate_recursive(alphabet, '', c)
+    return l
 
 def brute_force(d):
     length = 1
@@ -70,38 +91,21 @@ def brute_force(d):
                 final_state.append(d.run_with_input_list(inp_program))
             #print(final_state)
             if(len(set(final_state)) == 1):
-                print("congra!!")
                 return input
         length += 1
     return None
 
-def DFA_generator(num_states, num_alphabet):
-    states = [i for i in range(num_states)]
-    alphabet = list(string.ascii_lowercase[0:num_alphabet])
-
-    tf = dict()
-    for i in states:
-        for j in alphabet:
-            tf[(i, j)] = random.choice(states)
-
-    return DFA(states, alphabet, tf, 0, states)
-
-
-def SAT_Based(d, length):
+def CNF_gen(d, length):
 
     alphabet = d.alphabet
     num_alphabet = len(alphabet)
     num_state = len(d.states)
-
-
-    #length = 9
 
     #constraint 1
 
     constraint_1 = []
 
     X = [[Symbol('X' + str(i) + j) for j in alphabet] for i in range(length)]
-
 
     for i in range(length):
         constraint_1.append(Or(X[i]))
@@ -116,7 +120,7 @@ def SAT_Based(d, length):
     constraint_2 = []
 
     S = [[[Symbol('S' + str(i) + str(j) + str(k)) for k in range(num_state)] for j in range(length+1)] for i in range(num_state)]
-    #print(S)
+
     for i in range(num_state):
         for j in range(length+1):
             constraint_2.append(Or(S[i][j]))
@@ -125,7 +129,8 @@ def SAT_Based(d, length):
                     p = Or(Not(S[i][j][k]), Not(S[i][j][l]))
                     constraint_2.append(p)
     c2 = And(constraint_2)
-    #print(c2)
+
+    #constraint 3
 
     constraint_3 = []
     for i in range(num_state):
@@ -136,8 +141,6 @@ def SAT_Based(d, length):
 
     constraint_4 = []
 
-    #print(d.transition_function[(0, 'a')])
-
     for i in range(num_state):
         for j in range(length):
             for k in range(num_state):
@@ -145,10 +148,7 @@ def SAT_Based(d, length):
                     tran_func = (k, alphabet[l])
                     next_state = d.transition_function[tran_func]
                     #print(S[i][j][k], X[j][l], S[i][j+1][next_state])
-                    A = S[i][j][k]
-                    B = X[j][l]
-                    C = S[i][j+1][next_state]
-                    p = Or(Not(A), Not(B), C)
+                    p = Or(Not(S[i][j][k]), Not(X[j][l]), S[i][j+1][next_state])
                     constraint_4.append(p)
 
     c4 = And(constraint_4)
@@ -164,7 +164,6 @@ def SAT_Based(d, length):
     		p = Or(Not(Y[i]), Not(Y[j]))
     		constraint_5.append(p)
 
-
     c5 = And(constraint_5)
 
     #constraint 6
@@ -173,15 +172,16 @@ def SAT_Based(d, length):
         for j in range(num_state):
             p = Or(Not(Y[i]), S[j][length][i])
             constraint_6.append(p)
-    c6 = And(constraint_6)
 
+    c6 = And(constraint_6)
 
     CNF = And(c1, c2, c3, c4, c5, c6)
 
     model = get_model(CNF)
     synchronizing_words = []
     if(not model):
-        print("not Satisfied")
+        #print("not Satisfied")
+        pass
     else:
         for i in range(length):
             for j in range(num_alphabet):
@@ -189,40 +189,47 @@ def SAT_Based(d, length):
                 if(str(model[X[i][j]]) == 'True'):
                     synchronizing_words.append(alphabet[j])
         return ''.join(synchronizing_words)
+    return None
 
-'''
-states = [0, 1, 2]
-alphabet = ['a', 'b']
+def SAT_based(d):
+    solution = None
+    for i in range(1, (len(d.states)-1)**2 + 1):
+        solution = CNF_gen(d, i)
+        if(solution != None):
+            break
 
-tf = dict()
+    return solution
 
-tf[(0, 'a')] = 1
-tf[(0, 'b')] = 0
-tf[(1, 'a')] = 2
-tf[(1, 'b')] = 1
-tf[(2, 'a')] = 0
-tf[(2, 'b')] = 0
-'''
-l = []
-for line in sys.stdin:
-    l.append(line.strip().split(' '))
+def main():
+    #read dfa from input
+    l = []
+    for line in sys.stdin:
+        l.append(line.strip().split(' '))
 
-num_state = int(l[0][0])
-num_alphabet = int(l[0][1])
+    num_state = int(l[0][0])
+    num_alphabet = int(l[0][1])
 
-states = [i for i in range(num_state)]
-alphabet = list(string.ascii_lowercase[0:num_alphabet])
+    states = [i for i in range(num_state)]
+    alphabet = list(string.ascii_lowercase[0:num_alphabet])
 
-tf = dict();
+    tf = dict();
 
-for i in range(1, len(l)):
-    tran_func = (int(l[i][0]), l[i][1])
-    tf[tran_func] = int(l[i][2])
+    for i in range(1, len(l)):
+        tran_func = (int(l[i][0]), l[i][1])
+        tf[tran_func] = int(l[i][2])
 
-#dfa = DFA_generator(10, 2)
-#print(dfa.transition_function)
+    d = DFA_generator(20, 2)
+    #d = DFA(states, alphabet, tf, 0, states)
 
+    time0 = time.time()
+    pp = SAT_based(d)
+    time1 = time.time()
+    print(pp, time1 - time0)
 
-d = DFA(states, alphabet, tf, 0, states)
-k = SAT_Based(d, 9)
-print(k)
+    time0 = time.time()
+    aa = brute_force(d)
+    time1 = time.time()
+    print(aa, time1 - time0)
+
+if __name__== "__main__":
+  main()
